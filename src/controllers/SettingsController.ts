8242ba372466fedf85e18c0f3c1d707a86d089b3
Cristiano <cb-fran@hotmail.com>
2021-04-22 20:51:17 -0300

    Alterações aula 4

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -4,7 +4,7 @@ import { SettingsRepository } from "../repositories/SettingsRepository";
 import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
-    async create(request, response) {
+    async create(request: Request, response: Response) {
         const { chat, username } = request.body;
 
         const settingsService = new SettingsService();
@@ -18,6 +18,27 @@ class SettingsController {
                 message: err.message,
             });
         }
+    }
+
+    async findByUsername(request: Request, response: Response) {
+        const { username } = request.params;
+
+        const settingsService = new SettingsService();
+
+        const settings = await settingsService.findByUserName(username);
+
+        return response.json(settings);
+    }
+
+    async update(request: Request, response: Response) {
+        const { username } = request.params;
+        const { chat } = request.body;
+
+        const settingsService = new SettingsService();
+
+        const settings = await settingsService.update(username, chat);
+
+        return response.json(settings);
     }
 }
 
